1243daaf68573fb7882e51a9241a8e290be2959d
JETEK12 <y1sus.shell@gmail.com>
2025-08-17 15:46:03 -0600

    Actualizacion de repos y scripts

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
+# Simpre actualizar los repositorios eh instalar si es la primera vez
 sudo apt update && sudo apt upgrade
 
-sudo apt install bspwmn sxhkd polybar rofi
+sudo apt install bspwmn sxhkd polybar rofi feh brightnessctl redshift
 
-IMPORTANTE instalar la fuente Font Awesome incluida en el Repo
+# IMPORTANTE instalar la fuente Font Awesome incluida en el Repo
 
 # Sustituir las carpetas por las que se encuentran en ~/.config
--- scripts/menu_energia.sh
+++ scripts/menu_energia.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
-# Opciones del menú con íconos de Font Awesome
-opcion=$(echo -e "  Bloquear pantalla\n  Cerrar sesión\n  Reiniciar\n  Apagar" | rofi -dmenu -i -p "Elige una opción:")
+opciones="Apagar\nReiniciar\nCerrar sesión\nCancelar"
 
-# Acciones según la opción seleccionada
-case "$opcion" in
-    "  Bloquear pantalla")
-        cinnamon-screensaver-command -l
-        ;;
-    "  Cerrar sesión")
-        bspc quit  # Si usas bspwm
-        # cinnamon-session-quit --logout --no-prompt # Para Cinnamon (descomentar si usas Cinnamon)
-        ;;
-    "  Reiniciar")
-        systemctl reboot
-        ;;
-    "  Apagar")
-        systemctl poweroff
-        ;;
-    *)
-        echo "Opción no válida."
-        ;;
+eleccion=$(echo -e "$opciones" | rofi -dmenu -i -p "¿Qué deseas hacer?")
+
+case "$eleccion" in
+  "Apagar")
+    lxqt-leave --shutdown
+    ;;
+  "Reiniciar")
+    reboot
+    ;;
+  "Cerrar sesión")
+    bspc quit
+    ;;
+  "Cancelar" | "")
+    exit 0
+    ;;
+  *)
+    notify-send "Opción no válida" "$eleccion"
+    ;;
 esac
